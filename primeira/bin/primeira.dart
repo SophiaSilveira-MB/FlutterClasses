174730d064412d7137a67f6e79e4188eb961f5ad
SophiaSilveira-MB <sophia.silveira@montebravo.com.br>
2022-11-04 15:43:18 -0300

    idee

--- primeira/bin/primeira.dart
+++ primeira/bin/primeira.dart
@@ -1,5 +1,7 @@
 import 'package:primeira/primeira.dart' as primeira;
 
-void main(List<String> arguments) {
-  print('Hello world: ${primeira.calculate()}!');
+void main() {
+  print('Eu adoro pudim');
 }
+
+
