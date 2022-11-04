0f0e3ed4b298d268a471c9134c80633107f30ca5
SophiaSilveira-MB <sophia.silveira@montebravo.com.br>
2022-11-04 15:21:30 -0300

    ide

---
+++ primeira/bin/primeira.dart
@@ -0,0 +1,5 @@
+import 'package:primeira/primeira.dart' as primeira;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primeira.calculate()}!');
+}
---
+++ primeira/test/primeira_test.dart
@@ -0,0 +1,8 @@
+import 'package:primeira/primeira.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
